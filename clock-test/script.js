bdc5b72d7a3530004dec68637df4abe8c530b742
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2023-11-25 19:30:38 -0300

    analog-clock documented
    # Changes
    * clock documented
    # TODO
    * digital-clock
    * numbers of the analog clock

--- clock-test/script.js
+++ clock-test/script.js
@@ -1,12 +1,14 @@
 // Global Attributes
-setInterval(start,1000)
 var time = 0;
-
 var pointer_hours= document.querySelector("div#hours")
 var pointer_minutes= document.querySelector("div#minutes")
 var pointer_seconds= document.querySelector("div#seconds")
 
 //Constructor
+/**
+ * used to update the clock based on the computer time at every 1s
+ */
+setInterval(start,1000)
 function start() {
     getTime();
     rotatePointer(pointer_hours     ,parseAngle(time.getHours()    ,12))
@@ -15,16 +17,28 @@ function start() {
 }
 
 //Methods
+
+//update global variable time based on the computer time
 function getTime() {
     time = new Date();
 }
 
-function parseAngle(actualTime,cicleMax) {
-    return (actualTime/cicleMax)*360
+/**used to convert time in angle (degs)
+ * @param {Number} actualTime the actual value of the cicle
+ * @param {Number} cicleMax the biggest value of the cicle
+ * @returns the current angle of the cicle
+ */
+function parseAngle(actualTime,cycleMax) {
+    return (actualTime/cycleMax)*360
 }
 
+/**
+ * used to rotate the pointers based on the degree
+ * @param {Element} pointer the pointer thats gonna rotate
+ * @param {Number} deg       the rotation in degrees
+ */
 function rotatePointer(pointer, deg) {
-    deg+=180 // +180degs because the clock start at the bottom 
+    deg+=180 // TODO: +180degs because the clock start at the bottom 
     pointer.style.transformOrigin = 'top left';
     pointer.style.transform = `rotate(${deg}deg)` 
     
