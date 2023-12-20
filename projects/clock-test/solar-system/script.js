1f62e1bd3c4ab5bd8128497a560e21528e99776d
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2023-12-20 08:13:08 -0300

    arrays added

--- projects/clock-test/solar-system/script.js
+++ projects/clock-test/solar-system/script.js
@@ -4,20 +4,47 @@ var c = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-function createStars() {
+var farawayStars = []
+function createFarawayStars() {
     for (let i=0;i<100;i++) {
         let x=Math.random()*canvas.width
         let y=Math.random()*canvas.height
-        createStar(x,y)
+        let star = {x,y}
+        farawayStars.push(star)
     }
 }
 
-function createStar(x,y) {
+var closeStars = []
+function createCloseStars() {
+    let x = canvas.width/2
+    let y =canvas.height/2
+    let radius = 40
+    let color = "yellow"
+    let star = {x,y,radius,color}
+    closeStars.push(star)
+}
+
+function drawStar(x,y,radius,color) {
     c.beginPath()
-    c.fillStyle = "white"
-    c.arc(x,y,1,0,Math.PI*2,false)
+    c.fillStyle = color
+    c.arc(x,y,radius,0,Math.PI*2,false)
     c.fill()
     c.closePath()
 }
 
-createStars()
+function animate() {
+    c.clearRect(0,0,canvas.width,canvas.height)
+    requestAnimationFrame(animate)
+
+    farawayStars.forEach(star => {
+        drawStar(star.x, star.y, 1, "white")
+    })
+
+    closeStars.forEach(star => {
+        drawStar(star.x,star.y,star.radius,star.color)
+    })
+}
+
+createFarawayStars()
+createCloseStars()
+animate()
