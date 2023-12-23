c8d01192e581671183bf03d7550811b1bef6c172
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2023-12-23 09:42:02 -0300

    ballrunner: moviment added to the player

--- projects/canvas/ballrunner/script.js
+++ projects/canvas/ballrunner/script.js
@@ -4,9 +4,31 @@ var c = canvas.getContext("2d")
 canvas.width = innerWidth
 canvas.height= innerHeight
 
+addEventListener("keydown",keyhandler)
+
+function keyhandler(event) {
+    switch (event.key) {
+        case 'w':
+            player.dy-=10
+            break;
+        case 's':
+            player.dy+=10
+            break;
+        case 'a':
+            player.dx-=10
+            break
+        case 'd':
+            player.dx+=10
+            break
+        break;
+    }
+}
+
 function Player() {
     this.x = 200
     this.y = 200
+    this.dx=0
+    this.dy=0
     this.radius = 50
     this.color = "red"
     
@@ -18,11 +40,30 @@ function Player() {
         c.stroke()
         c.closePath()
     }
+
+    this.update = function() {
+
+        if (this.x+this.radius+this.dx>canvas.width) {
+            this.dx = -this.dx
+        }
+
+        this.x+=this.dx
+        this.y+=this.dy
+        this.draw();
+    }
+
 }
 
 var player = new Player()
 function init() {
-    player.draw()
+    
+}
+function animate() {
+    c.clearRect(0,0,canvas.width,canvas.height)
+    requestAnimationFrame(animate)
+
+    player.update()
 }
 
 init()
+animate()
