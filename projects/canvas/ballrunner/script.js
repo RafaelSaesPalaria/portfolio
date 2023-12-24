db28ed5a364d931827e23d18e46c5872d79c7bd5
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2023-12-24 14:22:57 -0300

    enemy created

--- projects/canvas/ballrunner/script.js
+++ projects/canvas/ballrunner/script.js
@@ -35,11 +35,13 @@ function keyhandler(event) {
 class Circle {
     constructor() {
         this.x = 200
-        this.y = 200
         this.dx=0
         this.dy=0
-        this.radius = 50
+        this.radius = 30
+        this.y = canvas.height-this.radius
         this.color = "red"
+        this.gravity = 1.5
+        this.friction = 0
         this.speed = 5
     }
     
@@ -53,12 +55,7 @@ class Circle {
     }
 
     update() {
-        if (this.y+this.radius+this.dy>canvas.height || this.y - this.radius + this.dy < 0) {
-            this.dy = -this.dy
-        } else {
-            this.dy+=1
-        }
-
+        
         this.x+=this.dx
         this.y+=this.dy
         this.draw();
@@ -69,10 +66,37 @@ class Circle {
 class Enemy extends Circle {
     constructor() {
         super()
-        this.y = this.radius+Math.random()*(canvas.height-2*this.radius);
+        this.y = (canvas.height/2+this.radius+Math.random()*((canvas.height/2)-2*this.radius)); 
         this.x = canvas.width+this.radius
-        this.dx= -(Math.random()*30)
+        this.dx= -10-(Math.random()*30)
+        this.radius=25
         this.color="blue"
+    }
+    update() {
+        players.forEach(player => {
+
+        
+        if ((player.x - (this.x) - this.dx< this.radius+player.radius & player.x - (this.x) + this.dx>-this.radius-player.radius)&
+        (player.y - (this.y) - this.dy < this.radius+player.radius & player.y - (this.y) + this.dy>-this.radius-player.radius)) {
+            player.die()
+        }
+        })
+
+        if (this.x+this.radius<0 || this.y+this.radius>canvas.height) {
+            if (Math.random()>0.5) {
+                this.y = (canvas.height/2+this.radius+Math.random()*((canvas.height/2)-2*this.radius));
+                this.x = canvas.width+this.radius
+                this.dx= -10-(Math.random()*30)
+                this.dy = 0
+            } else {
+                this.x = (this.radius+Math.random()*((canvas.width)-2*this.radius));
+                this.y = 0
+                this.dy= +10+(Math.random()*20)
+                this.dx = 0
+            }
+        }   
+
+        super.update()
     }
 }
 
@@ -80,6 +104,7 @@ class Player extends Circle {
     constructor() {
         super()
         this.color="red"
+        this.maxSpeed = 30
     }
 
     keyboardMoviment() {
@@ -91,9 +116,9 @@ class Player extends Circle {
 
         }
 
-        if (left & !right) {
+        if (left & !right & (this.dx>-this.maxSpeed)) {
             this.dx-=this.speed/2
-        } else if (!left & right) {
+        } else if (!left & right & (this.dx<this.maxSpeed)) {
             this.dx+=this.speed/2
         } else {
             
@@ -109,31 +134,48 @@ class Player extends Circle {
             this.dx+=1
         }
 
-        if (this.dy<0) {
-            this.dy+=1
-        }
-
         if (this.x+this.radius+this.dx>canvas.width || this.x - this.radius + this.dx < 0) {
             this.dx = -this.dx
+        }
+
+        if (this.y+this.radius+this.dy>canvas.height || this.y - this.radius + this.dy < 0) {
+            this.dy = -this.dy * this.friction
+        } else {
+            this.dy+=this.gravity
         }
 
         super.update()
     }
 
+    die() {
+        this.color = "purple"
+    }
+
 }
 
-var player = new Player()
-var enemy= new Enemy()
 
-function init() {}
+
+var enemys = []
+var players = []
+function init() {
+    players.push(new Player())
+    enemys.push(new Enemy())
+    enemys.push(new Enemy())
+}
 function enemySpawn() {}
 
 function animate() {
     c.clearRect(0,0,canvas.width,canvas.height)
     requestAnimationFrame(animate)
 
-    player.update()
-    enemy.update()
+    enemys.forEach(enemy =>{
+        enemy.update()
+    })
+
+    players.forEach(player =>{
+        player.update()
+    })
+
 }
 
 init()
