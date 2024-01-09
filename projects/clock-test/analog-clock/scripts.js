f600e4f4b3fa6dae6007565d0c4eb4561ab418f3
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-01-09 08:27:36 -0300

    point 1 draw

--- projects/clock-test/analog-clock/scripts.js
+++ projects/clock-test/analog-clock/scripts.js
@@ -4,7 +4,10 @@ canvas.width = innerWidth
 canvas.height= innerHeight
 
 var timeInSeconds = 0
-
+var clockLocation = {
+    x: canvas.width/2,
+    y: canvas.height/3*1
+}
 
 function getTime() {
     let hours = new Date().getHours()
@@ -16,26 +19,49 @@ function getTime() {
     console.log(timeInSeconds)
 }
 
+function updateTime() {
+
+}
+
 function drawClock() {
-    let x = canvas.width/2
-    let y = canvas.height/3*1
+    let x = clockLocation.x
+    let y = clockLocation.y
     let radius = 120
 
     c.beginPath()
+    c.fillStyle = "gray"
     c.arc(x,y,radius,0,Math.PI*2,false)
     c.fill()
     c.closePath()
 }
 
-function drawPointer() {
+class Pointer {
+    constructor(width, thichness, color) {
+        this.x = clockLocation.x
+        this.y = clockLocation.y
+        this.width = width
+        this.height = thichness
+        this.color = color
+    }
+    draw() {
+        c.beginPath()
+        c.fillStyle = this.color
+        c.fillRect(this.x,this.y,this.width,this.height)
+        c.closePath()
+    }
+    update() {
 
+        this.draw()
+    }
 }
 
+var p1 = new Pointer(100,10,"red");
 function animate() {
     c.clearRect(0,0,canvas.width,canvas.height)
     requestAnimationFrame(animate)
 
     drawClock()
+    p1.update()
 }
 
 getTime()
