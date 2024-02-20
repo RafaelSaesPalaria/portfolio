b2efc693b966f74cd4e22bf3f80e76726af346b3
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-02-20 16:59:41 -0300

    SunbasedClock: stars.js created

--- projects/sunbased-clock/scripts/script.js
+++ projects/sunbased-clock/scripts/script.js
@@ -1,19 +1,9 @@
 //Global Attributes
+
+import { updateStar, updateBackground, stars } from "./star.js";
+
 export var intervalSpeed = 1000
 var degree = 0
-
-var stars = {
-    sun: {
-        element: document.querySelector("div#sun"),
-        degPlus: 90,
-        distance: 350
-    },
-    moon: {
-        element: document.querySelector("div#moon"),
-        degPlus: 270,
-        distance: 300
-    }
-}
 
 import { timeSpeed, timeDirection, time} from "./controls.js"
 import { updateDigitalClock } from "./digital_clock.js"
@@ -55,7 +45,7 @@ function updateTime() {
  * Do: get the angle based on the time
  * @returns the degree of the angle
 */
-function getAngle() {
+export function getAngle() {
     let dayTime_inSeconds = get_dayTime_inSeconds()
                         
     degree = parseAngle(dayTime_inSeconds,(max.hours*max.minutes*max.seconds))   
@@ -75,29 +65,3 @@ function get_dayTime_inSeconds() {
     )
 }
 
-/**
- * Called: at every clock-second
- * Do: move the star to the currently time-location
- * @param {Object}   star        the star that's gonna be moved      
- * @param {Number} degPlus the plus factor in the degree calc 
- * @param {Number} radius    the distance of the star from the center of the orbit
- */
-function updateStar(star, degPlus, radius) {
-    let deg = getAngle();
-    let cardinal = getCardinalCoordinates((deg+degPlus)%max.degree,radius)
-    star.style.top = `calc(100% + ${cardinal.x}px)`
-    star.style.left = `calc(50% + ${cardinal.y}px)`
-}
-
-/**
- * Called: At every clock-second
- * Do: Update the background
- */
-function updateBackground() {
-    let sun = stars.sun.element;
-
-    let sun_x = sun.getBoundingClientRect().left+sun.clientWidth/2;
-    let sun_y = sun.getBoundingClientRect().top+sun.clientHeight/2;
-
-    document.body.style.backgroundImage = `radial-gradient(circle at ${sun_x}px ${sun_y}px , yellow, orange 10%, lightblue 35%, blue 95%)`
-}
