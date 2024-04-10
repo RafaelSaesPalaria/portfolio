a02ca1f91c5e8a8ec136a6106ffe08832b9d17e8
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-04-10 10:13:05 -0300

    documentation added

--- main-site/script.js
+++ main-site/script.js
@@ -6,7 +6,8 @@ let content = {
 }
 
 /**
- * 
+ * @Called When the menu icon is clicked
+ * @Do toggle the itens visibility
  */
 function onClickMenu() {
     if (menuDisplay) {
@@ -17,6 +18,10 @@ function onClickMenu() {
     menuDisplay = !menuDisplay;
 }
 
+/**
+ * @Called When the screen is resized
+ * @Do show de itens and hide the icon button the its wide then a mobile screen
+ */
 function resize() {
     if (window.innerWidth>=768) {
         showItems(true);
@@ -27,16 +32,31 @@ function resize() {
     }
 }
 
+/**
+ * @Called When the icon change (resize/click)
+ * @Do apply the change to all of the items
+ * @param {boolean} show if it shows the icons
+ */
 function showItems(show) {
     content.items.forEach(element => {
         showElement(element,show)
     });
 }
 
+/**
+ * @Called When a the screen is resized or the icon is clicked
+ * @Do Change the visibility of a element
+ * @param {HTMLElement} element 
+ * @param {boolean} show 
+ */
 function showElement(element, show) {
     element.style.display = show ? "block" : "none"
 }
 
+/**
+ * @Called at the start of the program
+ * @Do Initialize the methods/listeners
+ */
 init()
 function init() {
     resize()
