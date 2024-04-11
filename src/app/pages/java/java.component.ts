352f57b69f8320ca1237dac933276c3567c4fc4b
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-04-11 18:45:25 -0300

    projects.json created

---
+++ src/app/pages/java/java.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-java',
+  standalone: true,
+  imports: [],
+  templateUrl: './java.component.html',
+  styleUrl: './java.component.css'
+})
+export class JavaComponent {
+
+}
---
+++ src/app/pages/web/web.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-web',
+  standalone: true,
+  imports: [],
+  templateUrl: './web.component.html',
+  styleUrl: './web.component.css'
+})
+export class WebComponent {
+
+}
