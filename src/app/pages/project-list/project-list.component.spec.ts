d49cbda0578ec4ef2ab4595e521f017cf10ce034
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2024-04-17 12:30:00 -0300

    project-item removed

--- src/app/pages/project-list/project-list.component.spec.ts
+++ src/app/pages/project-list/project-list.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProjectItemComponent } from './project-item.component';
+import { ProjectListComponent } from './project-list.component';
 
-describe('ProjectItemComponent', () => {
-  let component: ProjectItemComponent;
-  let fixture: ComponentFixture<ProjectItemComponent>;
+describe('ProjectListComponent', () => {
+  let component: ProjectListComponent;
+  let fixture: ComponentFixture<ProjectListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProjectItemComponent]
+      imports: [ProjectListComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(ProjectItemComponent);
+    fixture = TestBed.createComponent(ProjectListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
