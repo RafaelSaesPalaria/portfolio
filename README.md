8d3e9d649e4370863c8991eb28c1bab0a5eb8edc
RafaelSaesPalaria <rafaelsaespalaria@gmail.com>
2026-01-08 23:35:06 -0300

    esboço

--- README.md
+++
@@ -1,4 +0,0 @@
-# web-tests
-This is a personal web environment to show my abilities and test new things.
-
-(Summary)[https://rafaelsaespalaria.github.io/web-test/]
--- docs/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-JTYCHFFM.mjs";import"./chunk-X6F56M6H.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
